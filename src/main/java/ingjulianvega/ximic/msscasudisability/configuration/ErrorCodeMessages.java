8964245c9dffae9d4a2b4a52cf787b8653ff254f
Julian Vega <ingjulianvega@gmail.com>
2021-07-18 12:53:04 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasudisability/configuration/ErrorCodeMessages.java
+++ src/main/java/ingjulianvega/ximic/msscasudisability/configuration/ErrorCodeMessages.java
@@ -6,7 +6,7 @@ import lombok.experimental.UtilityClass;
 public class ErrorCodeMessages {
 
     public static final String DISABILITY_NOT_FOUND_API_CODE = "401";
-    public static final String DISABILITY_NOT_FOUND_ERROR = "BODY_PART_NOT_FOUND";
+    public static final String DISABILITY_NOT_FOUND_ERROR = "BDISABILITY_NOT_FOUND";
     public static final String DISABILITY_NOT_FOUND_MESSAGE = "No se encontró la incapacidad";
     public static final String DISABILITY_NOT_FOUND_SOLUTION = "Verifique los datos";
 
